cabc6d206691fcc81c827b07dd1fe7ce058a501b
Ken Norris <kjnorris69@me.com>
2015-05-04 12:58:37 -0400

    JHU Exploratory Data Analysis Project 1 - plot1.png (output) and plot1.R (code to generate)

---
+++ plot1.R
@@ -0,0 +1,39 @@
+# Load libraries used in the script
+library(dplyr)
+library(lubridate)
+
+# Specify file from Data directory
+powerFile <- paste(getwd(), "Data",
+                   "household_power_consumption.txt", sep = "/")
+
+# Set column headings for the data frame
+powerCols <- c("Date", "Time", "GlobalActivePower", "GlobalReactovePower",
+               "Voltage", "GlobalIntensity", "SubMetering1",
+               "SubMetering2", "SubMetering3")
+
+# Load power consumption data into powerData data frame
+powerData <- read.table(powerFile, header = TRUE, sep = ";",
+                        col.names = powerCols, na.strings = "?",
+                        stringsAsFactors = FALSE)
+
+# Combine date and time and format as POSIX using lubridate
+powerData$DateTime <- dmy_hms(paste(powerData$Date,
+                                    powerData$Time, sep = " "))
+
+# Filter to 2007-02-01 and 2007-02-02
+selectPower <- filter(powerData, powerData$DateTime >= "2007-02-01",
+                      powerData$DateTime <= "2007-02-02")
+
+# Save plot as png - 480x480 pixels
+png(filename = paste(getwd(), plot1.png, sep = "/"),
+    bg = "white", width = 480, height = 480, units = "px")
+
+# Plot histogram
+hist(selectPower$GlobalActivePower, col = "Red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency")
+dev.off()
+
+
+
